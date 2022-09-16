9a9f928df7aa93582f71fad8374fda9d14456ac2
Ash-KODES <choudharykumarashutosh1209@gmail.com>
2022-09-16 11:07:26 +0530

    fucntion added

---
+++ 7_file_management/code.go
@@ -0,0 +1,23 @@
+// Files
+// File exists
+
+// Check if a file exists on your local disk
+// Can you check if a file exists on an external disk?
+// Read file
+
+// Think of when you’d read a file ‘line by line’ vs ‘at once’?
+// Create a new file containing names and read it into an array
+// Write file
+
+// Write a list of cities to a new file.
+// Rename file
+
+// Which package has the rename function?
+
+package main
+
+import "fmt"
+
+func main(){
+	
+}
---
+++ 9_function/code.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func print_till_10(){
+	for i:=0;i<10;i++{
+		fmt.Println(i+1)
+	}
+}
+
+func main(){
+	print_till_10()
+}
