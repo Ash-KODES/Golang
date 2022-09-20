4e56e6998273c032b8fe35fbf621246a76c6ea0b
Ash-KODES <choudharykumarashutosh1209@gmail.com>
2022-09-21 00:35:52 +0530

    code added

--- 1_hello_world/1_hello_world.go
+++ 1_hello_world/1_hello_world.go
@@ -1,16 +1,18 @@
 package main
+
 // always added
 
 import "fmt"
+
 //used to print things
 
 //entry point to code similar to c++
 func main() {
-	
+
 	fmt.Println("Ashutosh Kumar Choudhary")
 
 	fmt.Println("Bareilly")
 }
 
-//go run   code.go        --->for (compiling+running) 
-//go build code.go        --->for just compiling(will create a exe application)
+//go run   code.go        --->for (compiling+running)
+//go build code.go        --->for just compiling(will create an exe application)
